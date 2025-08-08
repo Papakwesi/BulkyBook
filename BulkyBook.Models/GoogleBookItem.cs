53b3db837e787f33e63410a73dde1d82a4956078
charles <charlestoku.125@gmail.com>
2025-08-08 10:27:07 +0000

    Stripe payment for testing completed and functional. Googlebooks API added for search functionality

---
+++ BulkyBook.Models/GoogleBookItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class GoogleBookItem
+    {
+        public string Title { get; set; }
+        public List<string> Authors { get; set; }
+        public string Description { get; set; }
+        public string Thumbnail { get; set; }
+        public string PreviewLink { get; set; }
+    }
+}
